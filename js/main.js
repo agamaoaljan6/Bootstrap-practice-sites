5207306c5bdc626dd057b5ff1854acbc481affe6
Aljan Agamao <agamaoaljan@gmail.com>
2019-09-16 15:05:36 -0700

    tried to refactor code

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 $(document).ready(function () {
-    $('.btn-sub1').hover(
-        function () {
-            $('.fa-angle-right').css({
-                "transform": "rotate(180deg)",
-                "margin-left": "5px"
-            });
-        },
-        function () {
-            $('.fa-angle-right').css({
-                "transform": "rotate(0deg)",
-                "margin-left": "0px"
-            });
-        }
-    )
-    $('.btn-sub2').hover(
-        function () {
-            $('.fa-angle-double-right').css({
-                "transform": "rotate(180deg)",
-                "margin-left": "5px"
-            });
-        },
-        function () {
-            $('.fa-angle-double-right').css({
-                "transform": "rotate(0deg)",
-                "margin-left": "0px"
-            });
-        }
-    );
+    // $('.btn-sub1').hover(
+    //     function () {
+    //         $('.fa-angle-right').css({
+    //             "transform": "rotate(180deg)",
+    //             "margin-left": "5px"
+    //         });
+    //     },
+    //     function () {
+    //         $('.fa-angle-right').css({
+    //             "transform": "rotate(0deg)",
+    //             "margin-left": "0px"
+    //         });
+    //     }
+    // )
+    // $('.btn-sub2').hover(
+    //     function () {
+    //         $('.fa-angle-double-right').css({
+    //             "transform": "rotate(180deg)",
+    //             "margin-left": "5px"
+    //         });
+    //     },
+    //     function () {
+    //         $('.fa-angle-double-right').css({
+    //             "transform": "rotate(0deg)",
+    //             "margin-left": "0px"
+    //         });
+    //     }
+    // );
 
     $('.content-1 .col-md-4 .circle').hover(
         function () {
